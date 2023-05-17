2ffef9350e73171338801f5148850a14bc8579b8
masumeh <khanimasume88@gmail.com>
2023-05-17 08:14:06 +0330

    add tailwind dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import UseMemo from "./components/UseMemo";
 
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <UseMemo />
+    </>
+  );
 }
 
 export default App
